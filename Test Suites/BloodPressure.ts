201644ce6e897f35d9a47bad79ac746cd6c0f507
jgunter <x00180702@mytudublin.ie>
2022-09-10 12:32:35 +0100

    Add Test Suite

---
+++ Test Suites/BloodPressure.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BloodPressure</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d1ed72ba-17bc-4c65-8c4e-539af515ccc4</testSuiteGuid>
+   <testCaseLink>
+      <guid>782c7831-17aa-4dff-a0c2-a3ee5b7eb78f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BloodPressureMono</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
