a08a8227a2fd566fa7ce88cc6fcc3cd4a9310545
John <x00180702@mytudublin.ie>
2022-09-30 11:20:39 +0100

    add runner scripts

--- Include/scripts/groovy/bloodPressureApp/CucumberRunner.groovy
+++ Include/scripts/groovy/bloodPressureApp/CucumberRunner.groovy
@@ -24,11 +24,7 @@ import cucumber.api.CucumberOptions
 import cucumber.api.junit.Cucumber
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-features= "Include/features",
-glue="", 
-plugin=["pretty", "html:ReportsFolder", "json:ReportsFolder/cucumber.json"],
-tags="@bpTest")
+@CucumberOptions(features= "Include/features",glue="", plugin=["pretty", "html:ReportsFolder", "json:ReportsFolder/cucumber.json"])
 
 
 public class CucumberRunner {
